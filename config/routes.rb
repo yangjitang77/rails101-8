09533762685fc803fc8316eee36b8c8a00982a24
yangjitang77 <yangjitang77@gmail.com>
2017-09-25 23:05:04 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
     resources :posts
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :account do
+    resources :groups
+  end
 end
